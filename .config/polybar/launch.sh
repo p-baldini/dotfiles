32fde860a9a0b298794396ebaa4706df50f66880
Paolo Baldini <paolobaldini01@gmail.com>
2020-05-06 23:34:04 +0200

    cleaned custom conf for actually fedora clean-gnome env

--- .config/polybar/launch.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# kill any running picom instances and re-run it
-killall -q picom >> /tmp/polybar.log 2>&1
-picom >> /tmp/polybar.log 2>&1 &
-
-# terminate already running bar instance and re-run it
-killall -q polybar
-polybar example >> /tmp/polybar.log 2>&1 &
