71b94a42c7456a23ab0d4b975402e9ec808b553b
Paolo Baldini <paolobaldini01@gmail.com>
2021-02-27 10:44:49 +0100

    added preview of docx files in ranger

--- .config/ranger/scope.sh
+++ .config/ranger/scope.sh
@@ -90,6 +90,9 @@ case "$extension" in
     # DOC Files
     doc)
     	try antiword "$path" && { dump | trim; exit 5; } || exit 1;;
+    # DOCX Files
+    docx)
+    	try docx2txt "$path" - && { dump | trim; exit 5; } || exit 1;;
     # HTML Pages:
     htm|html|xhtml)
         try w3m    -dump "$path" && { dump | trim | fmt -s -w $width; exit 4; }
