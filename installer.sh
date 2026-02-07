0f02d44207bbbd662ffeef3ca37fcc5c412ae355
Paolo Baldini <paolo.baldini.phd@gmail.com>
2026-02-07 13:31:19 +0100

    added scratch packages installer

---
+++ installer.sh
@@ -0,0 +1,31 @@
+# Install essential packages
+sudo pacman -S 7zip \
+    alacritty \
+    antiword \
+    bspwm \
+    docx2txt \
+    dunst \
+    fakeroot \
+    feh \
+    fontconfig \
+    noto-fonts \
+    odt2txt \
+    patch \
+    picom \
+    polybar \
+    ranger \
+    rofi \
+    scrot \
+    sxhkd \
+    unrar \
+    xdo \
+    zsh
+
+# Install work packages
+sudo pacman -S \
+    htop \
+    texlive-core
+
+# Install 'oh-my-zsh'
+yes n | sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
