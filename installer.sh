d53343fc309c323bdb7eb9b42d1b4ad52691968e
Paolo Baldini <paolo.baldini.phd@gmail.com>
2026-02-10 13:47:14 +0100

    solved backlight errors through installer script

--- installer.sh
+++ installer.sh
@@ -31,9 +31,21 @@ sudo pacman -S \
     code \
     htop \
     make \
+    texlive-binextra \
     texlive-core \
+    texlive-fontsextra \
+    texlive-lang \
+    texlive-latex \
+    texlive-latexextra \
+    texlive-latexrecommended \
+    texlive-plaingeneric \
     tree
+yay -S \
+    urw-classico
 
 # Install 'oh-my-zsh'
 yes n | sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Add user to video group (light tool)
+sudo usermod -a -G video $USER
 
